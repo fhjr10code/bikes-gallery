b0a225f38e7bd71ca948282668b1f50744c389c9
Floriberto Hernandez JR <fhjr10code@zoho.com>
2021-11-02 10:48:57 -0700

    Initialize React with TailWindCSS setup

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import './styles/main.css';
+
+const App = () => {
+    return (
+        <div>
+            <h1>
+                Hello
+                <span className='text-red-100'> W</span>
+                <span className='text-red-200'>o</span>
+                <span className='text-red-300'>r</span>
+                <span className='text-red-400'>l</span>
+                <span className='text-red-500'>d</span>
+                <span className='text-red-600'>!</span>
+            </h1>
+        </div>
+    );
+};
+
+export default App;
